e35f4ea0dd021adc80e8b9888ab97963ad947d90
APavlov <asd@asd.com>
2021-11-22 16:31:20 +0300

    Выполнено

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -13,9 +13,32 @@ def create_number():
     while d == a or d == b or d == c:
         d = randint(0, 9)
     secret_number = [a,b,c,d]
+    print(secret_number)
 
 
 def guess_the_number(client_number):
-    client_number = int(client_number)
-    cows = secret_number.count(client_number)
+    client_number = list(client_number)
+    cows = secret_number.count(int(client_number[0]))
+    cows += secret_number.count(int(client_number[1]))
+    cows += secret_number.count(int(client_number[2]))
+    cows += secret_number.count(int(client_number[3]))
+    bulls = 0
+    if secret_number[0] == int(client_number[0]):
+        bulls +=1
+    if secret_number[1] == int(client_number[1]):
+        bulls +=1
+    if secret_number[2] == int(client_number[2]):
+        bulls +=1
+    if secret_number[3] == int(client_number[3]):
+        bulls +=1
+    cows -= bulls
+    print(cows, bulls)
+
+
+
+create_number()
+guess_the_number('1234')
+
+
+
 
