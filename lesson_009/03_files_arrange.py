52af81e0b7ce1c1624d37ee646ed4f559f786140
Кусто <asd@asd.asd>
2022-01-11 19:50:15 +0300

    Выполнено

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -33,8 +33,44 @@ import os, time, shutil
 #
 # Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+from pprint import pprint
 
-# TODO здесь ваш код
+path = """E:\Torrent's KycTo\Python Обучение\[SkillBox] [Вадим Шандринов] Python-разработчик\9. Работа с файлами и форматированный вывод\lesson_009\icons"""
+new_path = """E:\Torrent's KycTo\Python Обучение\[SkillBox] [Вадим Шандринов] Python-разработчик\9. Работа с файлами и форматированный вывод\lesson_009\icons_by_year"""
+
+class Sort:
+
+    def __init__(self,old_path, new_path):
+        self.path = old_path
+        self.new_path = new_path
+        if not os.path.exists(new_path):
+            os.makedirs(name=new_path)
+
+
+    def create_dir(self, year_dir, month_dir):
+        new_dir = os.path.join(self.new_path, year_dir, month_dir)
+        if not os.path.exists(new_dir):
+            os.makedirs(name=new_dir)
+        return new_dir
+
+
+    def file_sort(self):
+        for dirpath, dirnames, filenames in os.walk(self.path):
+            for filename in filenames:
+                full_file_path = os.path.join(dirpath, filename)
+                secs = os.path.getmtime(full_file_path)
+                file_time = time.gmtime(secs)
+                year_dir = str(file_time[0])
+                month_dir = str(file_time[1])
+                new_dir = self.create_dir(year_dir=year_dir, month_dir=month_dir)
+                new_full_path = os.path.join(new_dir, filename)
+                shutil.copy2(src=full_file_path, dst=new_full_path)
+
+
+new_script = Sort(old_path=path, new_path=new_path)
+new_script.file_sort()
+
+
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
