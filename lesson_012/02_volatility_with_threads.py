d58582bbe32a359086c8844a803719a8337e479e
Кусто <asd@asd.asd>
2022-02-26 16:04:35 +0300

    Выполнено

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -35,24 +35,43 @@ class VolatilityAnalyzer(threading.Thread):
         self.path = path
         self.result_dict = {}
         self.zero_vol_list = []
+        self.volotility = 0
+        self.secid = None
     def run(self):
-        for dirpath, dirnames, filenames in os.walk(self.path):
-            for file in filenames:
-                full_path = os.path.join(dirpath, file)
-                secid, volotility = self._get_secid_volotility(path = full_path)
-                if volotility == 0.0:
-                    self.zero_vol_list.append(secid)
-                else:
-                    self.result_dict[secid] = volotility
-        sorted_dict = sorted(self.result_dict.items(), key=lambda item: item[1], reverse=True)
-        print (f' Максимальная волатильность:')
-        for item, value in sorted_dict[0:3]:
-            print (f'       {item}  : {value}%')
-        print(f' Минимальная волатильность:  ')
-        for item, value in sorted_dict[:-4:-1]:
-            print(f'       {item}  : {value}%')
-        # print(sorted_dict)
-        print('Нулевые тикеты', self.zero_vol_list)
+        # self.result_dict = {}
+        # self.zero_vol_list = []
+        if os.path.isdir(self.path):
+            for dirpath, dirnames, filenames in os.walk(self.path):
+                for file in filenames:
+                    full_path = os.path.join(dirpath, file)
+                    analyzers = [VolatilityAnalyzer(path=full_path)]
+                    for analyzer in analyzers:
+                        analyzer.start()
+                    for analyzer in analyzers:
+                        analyzer.join()
+                    for analyzer in analyzers:
+                        if analyzer.volotility == 0.0:
+                            self.zero_vol_list.append(analyzer.secid)
+                        else:
+                            self.result_dict[analyzer.secid] = analyzer.volotility
+        else:
+            self.secid, self.volotility = self._get_secid_volotility(path=self.path)
+
+
+        #
+        #
+        #              Вывод данных
+        #           надо перенести отдельно в мейн
+        # sorted_dict = sorted(self.result_dict.items(), key=lambda item: item[1], reverse=True)
+        # print (f' Максимальная волатильность:')
+        # for item, value in sorted_dict[0:3]:
+        #     print (f'       {item}  : {value}%')
+        # print(f' Минимальная волатильность:  ')
+        # for item, value in sorted_dict[:-4:-1]:
+        #     print(f'       {item}  : {value}%')
+        # print('Нулевые тикеты', self.zero_vol_list)
+        #
+        #
 
     def _get_secid_volotility(self, path):
         with open(file=path) as file:
@@ -77,6 +96,13 @@ class VolatilityAnalyzer(threading.Thread):
 def main():
     probe = VolatilityAnalyzer(path='trades')
     probe.run()
-
+    sorted_dict = sorted(probe.result_dict.items(), key=lambda item: item[1], reverse=True)
+    print(f' Максимальная волатильность:')
+    for item, value in sorted_dict[0:3]:
+        print(f'       {item}  : {value}%')
+    print(f' Минимальная волатильность:  ')
+    for item, value in sorted_dict[:-4:-1]:
+        print(f'       {item}  : {value}%')
+    print('Нулевые тикеты', probe.zero_vol_list)
 if __name__ == '__main__':
     main()
